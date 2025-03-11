8023c5d2eef6e68531061ac703a11f65f3180fce
Hussain Gaddal <hussain.yg2010@gmail.com>
2025-03-11 23:59:43 +0300

    Set up build-logic module

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    `kotlin-dsl`
+}
+
+group = "dev.gaddal.spendless.buildlogic"
+
+dependencies {
+    compileOnly(libs.android.gradlePlugin)
+    compileOnly(libs.android.tools.common)
+    compileOnly(libs.kotlin.gradlePlugin)
+    compileOnly(libs.ksp.gradlePlugin)
+    compileOnly(libs.room.gradlePlugin)
+}
---
+++ build-logic/convention/src/main/java/dev/gaddal/convention/MyClass.kt
@@ -0,0 +1,4 @@
+package dev.gaddal.convention
+
+class MyClass {
+}
